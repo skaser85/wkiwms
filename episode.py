086511d42d7487630c41f0d7746d54ddc4296b73
Steve Kaser <skaser85@gmail.com>
2022-11-15 03:18:12 +0000

    linting updates

--- episode.py
+++ episode.py
@@ -1,14 +1,18 @@
+"""
+The episode module is really cool.
+"""
+
 from __future__ import annotations
-from typing import List, Tuple
+from typing import List
 from dataclasses import dataclass, field, asdict
 from datetime import date
-from bs4 import Tag
-from dbhandler import DBHandler
 from pprint import pprint
+from dbhandler import DBHandler
 
 DB_PATH = 'wkiwms.db'
 
 def get_episodes() -> List[Episode]:
+    """ Gets the episodes from the db. """
     # episodes  = [
     #     {
     #         number,
@@ -86,23 +90,9 @@ def get_episodes() -> List[Episode]:
         # create list of dict episodes
         return [e.to_dict() for e in episodes]
 
-def recipe_exists(url: str) -> bool:
-    with DBHandler(DB_PATH) as db:
-        recipe = db.fetch_one(f'SELECT * FROM recipe WHERE url=?', (url,))
-        print(recipe)
-        return False if recipe is None else True
-
-def add_recipe(recipe: Recipe):
-    with DBHandler(DB_PATH) as db:
-        c = db.insert(f'INSERT INTO recipe (id,name,url,description) VALUES (?,?,?,?)', (None, recipe.name, recipe.url, recipe.description))
-        r_id = c.lastrowid
-        for ing in recipe.ingredients:
-            db.insert(f'INSERT INTO ingredient (recipe_id, name, amount, unit) VALUES (?,?,?,?)', (r_id, ing.name, ing.amount, ing.unit))
-        for inst in recipe.instructions:
-            db.insert(f'INSERT INTO instruction (recipe_id, sequence, text) VALUES (?,?,?)', (r_id, inst.order, inst.instruction))
-
 @dataclass
 class Answer:
+    """ Answer """
     id: int
     answer: str
     type: str
@@ -112,6 +102,7 @@ class Answer:
 
 @dataclass
 class Question:
+    """ Question """
     number: int
     question: str
     category: str
@@ -121,17 +112,20 @@ class Question:
 
 @dataclass
 class Guest:
+    """ Guest """
     id: int
     name: str
 
 @dataclass
 class Episode:
+    """ Episode """
     number: int
     record_date: date
     guests: List[Guest] = field(default_factory=list)
     questions: List[Question] = field(default_factory=list)
 
     def to_dict(self) -> dict:
+        """ Returns this Episode object as a dict """
         return asdict(self)
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -1,17 +1,16 @@
-from flask import Flask, render_template, request
-import requests
-from bs4 import BeautifulSoup
-from Episode import get_episodes
+"""
+This is the server file that serves the webpages and talks to the database.
+"""
+
+from flask import Flask, render_template
+from episode import get_episodes
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    """ serves the home page """
     return render_template('index.html', episodes=get_episodes())
 
-@app.route('/get-episodes')
-def get_episodes_():
-    return {'error': '', 'episodes': get_episodes()}
-
 if __name__ == '__main__':
-     app.run(host='0.0.0.0', port=8008, debug=False)
+     app.run(host='0.0.0.0', port=8008, debug=True)
